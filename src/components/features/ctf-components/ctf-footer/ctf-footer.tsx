cf2098873db73a8911d90f2e1061379cd576eeb5
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2024-08-02 22:58:18 +0200

    remove mui from footer

--- src/components/features/ctf-components/ctf-footer/ctf-footer.tsx
+++ src/components/features/ctf-components/ctf-footer/ctf-footer.tsx
@@ -3,8 +3,6 @@ import Facebook from '@mui/icons-material/Facebook';
 import Instagram from '@mui/icons-material/Instagram';
 import LinkedIn from '@mui/icons-material/LinkedIn';
 import Twitter from '@mui/icons-material/Twitter';
-import { Theme, Container, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import { useTranslation } from 'next-i18next';
 
 import { FooterFieldsFragment } from './__generated/ctf-footer.generated';
@@ -17,210 +15,6 @@ import { LanguageSelector } from '@src/components/features/language-selector';
 import { Link } from '@src/components/shared/link';
 import { useContentfulContext } from '@src/contentful-context';
 import Logo from '@src/icons/logo-tagline.svg';
-import { CONTAINER_WIDTH } from '@src/theme';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  footerContainer: {
-    backgroundColor: '#F4F4F4',
-  },
-  footer: {
-    display: 'flex',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    flexWrap: 'wrap',
-    maxWidth: `${CONTAINER_WIDTH / 10}rem`,
-    paddingBottom: theme.spacing(5),
-    paddingTop: theme.spacing(8),
-    [theme.breakpoints.up('sm')]: {
-      paddingBottom: theme.spacing(10),
-      paddingTop: theme.spacing(20),
-    },
-  },
-  menuWrapper: {
-    alignItems: 'flex-start',
-    display: 'flex',
-    flexWrap: 'wrap',
-    marginLeft: theme.spacing(-8),
-  },
-  menuColumn: {
-    paddingLeft: theme.spacing(8),
-  },
-  menu: {
-    listStyle: 'none',
-    margin: theme.spacing(0, 0, 8),
-    padding: 0,
-    width: '17.2rem',
-    [theme.breakpoints.up('md')]: {
-      marginBottom: 0,
-    },
-  },
-  menuItem: {
-    fontSize: '1.8rem',
-    fontWeight: 400,
-    lineHeight: 1.2,
-    color: '#1B273A',
-    margin: theme.spacing(0, 0, 4),
-    [theme.breakpoints.up('sm')]: {
-      marginBottom: theme.spacing(8),
-    },
-  },
-  submenu: {
-    listStyle: 'none',
-    margin: 0,
-    padding: 0,
-    '& $menuItem': {
-      fontWeight: 400,
-    },
-  },
-  submenuItem: {
-    '& a': {
-      borderBottom: '1px solid transparent',
-      color: '#414D63',
-      display: 'inline-block',
-      minWidth: 0,
-      transition: 'border-bottom-color 0.2s ease-in-out',
-    },
-
-    '&:hover, &:focus, &:focus-within': {
-      '& > a': {
-        borderBottomColor: '#7C7C7C',
-      },
-    },
-  },
-  footerEndSection: {
-    marginLeft: 'auto',
-  },
-  footerCorporateContainer: {
-    backgroundColor: '#212121',
-    color: '#fff',
-    paddingBottom: theme.spacing(14),
-    paddingTop: theme.spacing(8),
-  },
-  footerCorporate: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    maxWidth: `${CONTAINER_WIDTH / 10}rem`,
-  },
-  storeLogos: {
-    marginTop: theme.spacing(7),
-  },
-  storeLogo: {
-    display: 'block',
-    maxWidth: '11.5rem',
-    '& + &': {
-      marginTop: theme.spacing(5),
-    },
-    '& img': {
-      display: 'block',
-      maxWidth: '100%',
-    },
-  },
-  corporateLogoMenu: {
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-  },
-  corporateLogoContainer: {
-    flexShrink: 0,
-    marginBottom: theme.spacing(3),
-    marginTop: '0.2rem',
-    [theme.breakpoints.up('md')]: {
-      flexShrink: 0,
-      width: '38.4rem',
-    },
-  },
-  corporateLogo: {
-    display: 'block',
-    height: 'auto',
-    maxWidth: '100%',
-  },
-  copyrightAndLegal: {
-    [theme.breakpoints.up('md')]: {
-      alignItems: 'flex-start',
-      display: 'flex',
-    },
-  },
-  copyright: {
-    fontSize: '1.8rem',
-    lineHeight: 1.2,
-    margin: theme.spacing(1, 10, 0, 0),
-  },
-  legalMenuWrapper: {},
-  legalMenu: {
-    listStyle: 'none',
-    margin: theme.spacing(5, 0, 0),
-    padding: 0,
-    [theme.breakpoints.up('md')]: {
-      alignItems: 'flex-start',
-      display: 'flex',
-      flexWrap: 'wrap',
-      marginTop: 0,
-    },
-  },
-  legalMenuItem: {
-    fontSize: '2rem',
-    marginTop: theme.spacing(2),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      marginTop: 0,
-      width: 'auto',
-    },
-    '&:not(:last-child)': {
-      [theme.breakpoints.up('md')]: {
-        marginRight: theme.spacing(8),
-      },
-    },
-    '& a': {
-      borderBottom: '1px solid transparent',
-      color: '#fff',
-      display: 'inline-block',
-      transition: 'border-bottom-color 0.2s ease-in-out',
-    },
-
-    '&:hover, &:focus, &:focus-within': {
-      '& > a': {
-        borderBottomColor: '#000',
-      },
-    },
-  },
-  socialDisclaimer: {
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      marginTop: theme.spacing(7),
-    },
-  },
-  socialWrapper: {
-    [theme.breakpoints.up('md')]: {
-      flexShrink: 0,
-      order: -1,
-      marginRight: theme.spacing(8),
-      width: '38.4rem',
-    },
-  },
-  socialTitle: {
-    fontSize: '1.8rem',
-    fontWeight: 400,
-    lineHeight: 1.2,
-  },
-  social: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    marginTop: theme.spacing(6),
-    '& a': {
-      color: 'inherit',
-      display: 'inline-block',
-      lineHeight: 1.2,
-
-      '&:not(:first-child)': {
-        marginLeft: theme.spacing(6),
-      },
-    },
-    '& .MuiSvgIcon-root': {
-      fontSize: '3.2rem',
-    },
-  },
-}));
 
 export const CtfFooter = (props: FooterFieldsFragment) => {
   const footerContent = props.items[0];
@@ -242,7 +36,7 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
             fieldId: 'pageName',
           })}
         >
-          <Link href={href} className={classes.menuItem}>
+          <Link href={href} className={''}>
             {linkText}
           </Link>
         </li>
@@ -250,7 +44,6 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
     });
   };
 
-  const classes = useStyles();
   const containerProps = footerContent?.sys?.id
     ? inspectorMode({
         entryId: footerContent.sys.id,
@@ -261,18 +54,18 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
 
   return (
     <>
-      <Container {...containerProps} maxWidth={false} className={classes.footerContainer}>
-        <footer className={classes.footer}>
+      <div {...containerProps} className="">
+        <footer className="">
           {footerContent?.menuItemsCollection?.items?.length && (
-            <nav role="navigation" className={classes.menuWrapper}>
+            <nav role="navigation" className="">
               {footerContent.menuItemsCollection.items.map(
                 menuItem =>
                   menuItem && (
-                    <div key={menuItem.sys.id} className={classes.menuColumn}>
-                      <ul className={classes.menu}>
+                    <div key={menuItem.sys.id} className={''}>
+                      <ul className={''}>
                         <li>
                           <p
-                            className={classes.menuItem}
+                            className={''}
                             {...inspectorMode({
                               entryId: menuItem.sys.id,
                               fieldId: 'groupName',
@@ -282,10 +75,11 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
                             {menuItem.groupName}
                           </p>
                           {menuItem.featuredPagesCollection && (
-                            <ul className={classes.submenu}>
+                            <ul className={''}>
                               {renderMenuGroupLinks(
                                 menuItem.featuredPagesCollection,
-                                classes.submenuItem,
+
+                                '',
                               )}
                             </ul>
                           )}
@@ -296,39 +90,34 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
               )}
             </nav>
           )}
-          <section className={classes.footerEndSection}>
+          <section className={''}>
             <LanguageSelector />
           </section>
         </footer>
-      </Container>
-      <Container maxWidth={false} className={classes.footerCorporateContainer}>
-        <section className={classes.footerCorporate}>
-          <div className={classes.corporateLogoMenu}>
-            <div className={classes.corporateLogoContainer}>
-              <Logo className={classes.corporateLogo} />
+      </div>
+      <div className={''}>
+        <section className={''}>
+          <div className={''}>
+            <div className={''}>
+              <Logo className={''} />
             </div>
 
-            <section className={classes.copyrightAndLegal}>
-              <p className={classes.copyright}>
-                {t('legal.copyright', { year: new Date().getFullYear() })}
-              </p>
+            <section className={''}>
+              <p className={''}>{t('legal.copyright', { year: new Date().getFullYear() })}</p>
               {footerContent?.legalLinks?.featuredPagesCollection?.items?.length && (
-                <nav role="navigation" className={classes.legalMenuWrapper}>
-                  <ul className={classes.legalMenu}>
-                    {renderMenuGroupLinks(
-                      footerContent.legalLinks.featuredPagesCollection,
-                      classes.legalMenuItem,
-                    )}
+                <nav role="navigation" className={''}>
+                  <ul className={''}>
+                    {renderMenuGroupLinks(footerContent.legalLinks.featuredPagesCollection, '')}
                   </ul>
                 </nav>
               )}
             </section>
           </div>
 
-          <div className={classes.socialDisclaimer}>
-            <div className={classes.socialWrapper}>
-              <Typography className={classes.socialTitle}>{t('socials.findUsOn')}</Typography>
-              <div className={classes.social}>
+          <div className={''}>
+            <div className={''}>
+              <p className={''}>{t('socials.findUsOn')}</p>
+              <div className={''}>
                 {footerContent?.twitterLink && (
                   <a
                     href={footerContent.twitterLink}
@@ -373,7 +162,7 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
             </div>
           </div>
         </section>
-      </Container>
+      </div>
     </>
   );
 };
