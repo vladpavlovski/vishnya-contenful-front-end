1447a9b8b1ec336a4af2ff764412f7fdcc54d159
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2024-08-03 15:18:19 +0200

    update libs

--- src/components/features/markdown.tsx
+++ src/components/features/markdown.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import parse from 'rehype-parse';
-import rehypeReact from 'rehype-react';
-import breaks from 'remark-breaks';
-import { unified } from 'unified';
+import React from 'react'
+import * as prod from 'react/jsx-runtime'
+import parse from 'rehype-parse'
+import rehypeReact from 'rehype-react'
+import breaks from 'remark-breaks'
+import { unified } from 'unified'
 
-const renderer = unified()
-  .use(parse)
-  .use(breaks)
-  .use(rehypeReact, { createElement: React.createElement });
+const production = { Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs }
+// @ts-expect-error: the react types are missing.
+const renderer = unified().use(parse).use(breaks).use(rehypeReact, production)
 
 type Props = {
-  text: string;
-  className?: string;
-};
+  text: string
+  className?: string
+}
 
 export const Markdown = (props: Props) => {
   return (
     <div className={''} style={{ fontFamily: 'Arial, sans-serif', color: 'black' }}>
       {(renderer.processSync(props.text) as any).result}
     </div>
-  );
-};
+  )
+}
