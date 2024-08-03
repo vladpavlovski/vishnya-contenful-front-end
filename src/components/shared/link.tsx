44e6a53c31b136653ae042e8a43482fc53672432
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2024-08-03 16:10:50 +0200

    update links

--- src/components/shared/link.tsx
+++ src/components/shared/link.tsx
@@ -1,28 +1,28 @@
-import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-import queryString from 'query-string';
-import { ReactNode } from 'react';
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
+import queryString from 'query-string'
+import { ReactNode } from 'react'
 
 interface Props {
-  children: ReactNode;
-  href?: string;
-  as?: string;
-  target?: string;
-  dropUrlParams?: boolean;
-  className?: string;
-  withoutMaterial?: boolean;
-  underline?: boolean;
-  onClick?: () => any;
-  isButton?: boolean;
-  variant?: 'text' | 'outlined' | 'contained' | undefined;
-  size?: 'small' | 'medium' | 'large' | undefined;
-  color?: any;
-  startIcon?: any;
-  endIcon?: any;
-  urlParams?: string;
-  title?: string;
+  children: ReactNode
+  href?: string
+  as?: string
+  target?: string
+  dropUrlParams?: boolean
+  className?: string
+  withoutMaterial?: boolean
+  underline?: boolean
+  onClick?: () => any
+  isButton?: boolean
+  variant?: 'text' | 'outlined' | 'contained' | undefined
+  size?: 'small' | 'medium' | 'large' | undefined
+  color?: any
+  startIcon?: any
+  endIcon?: any
+  urlParams?: string
+  title?: string
 }
-export type LinkProps = Props;
+export type LinkProps = Props
 
 export const Link = (props: Props) => {
   const {
@@ -35,52 +35,52 @@ export const Link = (props: Props) => {
     isButton = false,
     color,
     urlParams = '',
-    title,
-  } = props;
-  const router = useRouter();
-  let href = props.href || '';
-  let { as } = props;
+    title
+  } = props
+  const router = useRouter()
+  let href = props.href || ''
+  let { as } = props
 
   if (!dropUrlParams && router) {
-    const urlQuerystring = router.asPath.split('?')[1];
+    const urlQuerystring = router.asPath.split('?')[1]
     if (urlQuerystring) {
-      href += href.indexOf('?') < 0 ? `?${urlQuerystring}` : `&${urlQuerystring}`;
+      href += href.indexOf('?') < 0 ? `?${urlQuerystring}` : `&${urlQuerystring}`
     }
   }
 
   if (urlParams !== '') {
-    const parsedUrlParams = queryString.parse(urlParams);
-    const parsedHref = queryString.parseUrl(href);
+    const parsedUrlParams = queryString.parse(urlParams)
+    const parsedHref = queryString.parseUrl(href)
 
     const mergedParsedHref = {
       ...parsedHref,
       query: {
         ...parsedHref.query,
-        ...parsedUrlParams,
-      },
-    };
+        ...parsedUrlParams
+      }
+    }
 
-    href = queryString.stringifyUrl(mergedParsedHref);
+    href = queryString.stringifyUrl(mergedParsedHref)
 
     if (as !== undefined) {
-      const parsedAs = queryString.parseUrl(as);
+      const parsedAs = queryString.parseUrl(as)
 
       const mergedParsedAs = {
         ...parsedAs,
         query: {
           ...parsedAs.query,
-          ...parsedUrlParams,
-        },
-      };
+          ...parsedUrlParams
+        }
+      }
 
-      as = queryString.stringifyUrl(mergedParsedAs);
+      as = queryString.stringifyUrl(mergedParsedAs)
     }
   }
 
-  if (props.href === undefined || props.href === null) return <>{props.children}</>;
+  if (props.href === undefined || props.href === null) return <>{props.children}</>
 
-  const external = href.startsWith('http://') || href.startsWith('https://');
-  const underlineStyle = underline ? 'always' : 'none';
+  const external = href.startsWith('http://') || href.startsWith('https://')
+  const underlineStyle = underline ? 'always' : 'none'
 
   if (external === true || !href) {
     return isButton ? (
@@ -104,22 +104,20 @@ export const Link = (props: Props) => {
       >
         {children}
       </a>
-    );
+    )
   }
 
   if (withoutMaterial === true) {
     return (
-      <NextLink href={href} as={as} passHref>
-        <a className={''} title={title}>
-          {children}
-        </a>
+      <NextLink href={href} as={as} passHref className={''} title={title}>
+        {children}
       </NextLink>
-    );
+    )
   }
 
   if (isButton === true) {
     return (
-      <NextLink href={href} as={as} passHref>
+      <NextLink href={href} as={as} passHref legacyBehavior>
         <button
           className={className}
           style={{ color: color }}
@@ -129,21 +127,22 @@ export const Link = (props: Props) => {
           {children}
         </button>
       </NextLink>
-    );
+    )
   }
 
   return (
-    <NextLink href={href} as={as} passHref>
-      <a
-        role="link"
-        tabIndex={0}
-        className={className}
-        style={{ textDecoration: underlineStyle, color: color }}
-        onClick={() => onClick && onClick()}
-        title={title}
-      >
-        {children}
-      </a>
+    <NextLink
+      href={href}
+      as={as}
+      passHref
+      role="link"
+      tabIndex={0}
+      className={className}
+      style={{ textDecoration: underlineStyle, color: color }}
+      onClick={() => onClick && onClick()}
+      title={title}
+    >
+      {children}
     </NextLink>
-  );
-};
+  )
+}
