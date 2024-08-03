22a2086af70ad9fd543b22f275fec9b739ea1ce5
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2024-08-03 15:22:51 +0200

    update libs

--- src/components/features/author/author.tsx
+++ src/components/features/author/author.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-import { Avatar } from '@src/components/features/avatar';
-import { PersonFieldsFragment } from '@src/components/features/ctf-components/ctf-person/__generated/ctf-person.generated';
+import { Avatar } from '@src/components/features/avatar'
+import { PersonFieldsFragment } from '@src/components/features/ctf-components/ctf-person/__generated/ctf-person.generated'
 
-interface AuthorPropsInterface extends PersonFieldsFragment {}
-
-export const Author = (props: AuthorPropsInterface) => {
-  const { name, avatar } = props;
+export const Author = (props: PersonFieldsFragment) => {
+  const { name, avatar } = props
 
   return (
     <div>
@@ -22,12 +20,12 @@ export const Author = (props: AuthorPropsInterface) => {
             fontSize: '2.5rem',
             lineHeight: 1.52,
             marginBottom: 0,
-            marginTop: '3rem',
+            marginTop: '3rem'
           }}
         >
           {name}
         </p>
       )}
     </div>
-  );
-};
+  )
+}
